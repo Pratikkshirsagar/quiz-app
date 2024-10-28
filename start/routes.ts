f667b795e70d5e670586bf799c500c675f3ee440
pratikkshirsagar <meetpratiik@gmail.com>
2024-10-28 22:25:58 +0530

    misc fix

--- start/routes.ts
+++ start/routes.ts
@@ -10,15 +10,9 @@
 import router from '@adonisjs/core/services/router'
 const QuizzesController = () => import('#controllers/quizzes_controller')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
 router.group(() => {
   router.get('/quiz/:id', [QuizzesController, 'getQuiz'])
   router.post('/quiz', [QuizzesController, 'storeQuiz'])
-  router.post('/selected-option', [QuizzesController, 'storeUserSelectedAnswer'])
-  router.post('/user-result', [QuizzesController, 'getUserQuizResults'])
+  router.post('/quiz/answer', [QuizzesController, 'storeUserSelectedAnswer'])
+  router.post('/quiz/result', [QuizzesController, 'getUserQuizResults'])
 })
